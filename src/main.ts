2a7ea533ae42968add0e65e0618b061f687761dd
LucasBilbao <lucasbm789@gmail.com>
2025-05-25 22:13:10 +0400

    feat: add new origin

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['https://luka-s-cv-6dde7.web.app'],
+    origin: ['https://lucasbilbao.github.io'],
     methods: ['GET'],
     credentials: true,
   });
